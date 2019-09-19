d2643380b1045bf798dac53ebc3893e6c6baa6dd
nthDerivative <bhirosky@outlook.com>
2019-09-19 16:43:47 -0400

    Added Projects

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const express = require('express');
+const people = require("./projects.json");
+
+const app = express();
+
+app.set("view engine", "pug");
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App Test
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello world\n');
+// App
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Brian Hirosky's Resume",
+        projects: projects.profiles
+    });
+});
+
+app.get("/profile", (req, res) => {
+    const project = project.profiles.find(p => p.id === req.query.id);
+    res.render("profile", {
+        title: `About ${project.name}`,
+        project
+    });
 });
 
 app.listen(PORT, HOST);
